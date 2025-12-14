99c91642ac27490ae0159b055c08c91d2f54b4cc
unknown <vinhgiauten3@gmail.com>
2025-12-14 23:47:08 +0700

    thêm class themmeinfo

---
+++ Models/ThemeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Do_an.Models
+{
+    public class ThemeInfo
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string VideoUrl { get; set; }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public Image ThumbnailImg { get; set; }
+    }
+}
