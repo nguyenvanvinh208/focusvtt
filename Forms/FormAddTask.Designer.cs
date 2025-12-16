697a986cc760a4df58c1040e902b329b9ddd88eb
Hoàng Công Thọ <hoangcongtho345@gmail.com>
2025-12-17 00:11:30 +0700

    update-firebase

--- Forms/FormAddTask.Designer.cs
+++ Forms/FormAddTask.Designer.cs
@@ -2,38 +2,203 @@
 {
     partial class FormAddTask
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.pnlName = new System.Windows.Forms.Panel();
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.pnlStart = new System.Windows.Forms.Panel();
+            this.lblHeaderStart = new System.Windows.Forms.Label();
+            this.pnlEnd = new System.Windows.Forms.Panel();
+            this.lblHeaderEnd = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlName.SuspendLayout();
+            this.pnlStart.SuspendLayout();
+            this.pnlEnd.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(0, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(550, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Thêm Công Việc";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.BackColor = System.Drawing.Color.Transparent;
+            this.lblClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblClose.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.lblClose.ForeColor = System.Drawing.Color.White;
+            this.lblClose.Location = new System.Drawing.Point(500, 10);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(33, 37);
+            this.lblClose.TabIndex = 1;
+            this.lblClose.Text = "✕";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // pnlName
+            // 
+            this.pnlName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(20)))), ((int)(((byte)(40)))));
+            this.pnlName.Controls.Add(this.txtTaskName);
+            this.pnlName.Location = new System.Drawing.Point(40, 80);
+            this.pnlName.Name = "pnlName";
+            this.pnlName.Size = new System.Drawing.Size(470, 55);
+            this.pnlName.TabIndex = 2;
+            this.pnlName.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlName_Paint);
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(20)))), ((int)(((byte)(40)))));
+            this.txtTaskName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTaskName.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.txtTaskName.ForeColor = System.Drawing.Color.Gray;
+            this.txtTaskName.Location = new System.Drawing.Point(55, 14);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(390, 30);
+            this.txtTaskName.TabIndex = 0;
+            this.txtTaskName.Text = "Tên công việc";
+            this.txtTaskName.Enter += new System.EventHandler(this.txtTaskName_Enter);
+            this.txtTaskName.Leave += new System.EventHandler(this.txtTaskName_Leave);
+            // 
+            // pnlStart
+            // 
+            this.pnlStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(20)))), ((int)(((byte)(40)))));
+            this.pnlStart.Controls.Add(this.lblHeaderStart);
+            this.pnlStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pnlStart.Location = new System.Drawing.Point(40, 160);
+            this.pnlStart.Name = "pnlStart";
+            this.pnlStart.Size = new System.Drawing.Size(220, 90);
+            this.pnlStart.TabIndex = 3;
+            this.pnlStart.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlTime_Paint);
+            // 
+            // lblHeaderStart
+            // 
+            this.lblHeaderStart.AutoSize = true;
+            this.lblHeaderStart.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeaderStart.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblHeaderStart.ForeColor = System.Drawing.Color.LightGray;
+            this.lblHeaderStart.Location = new System.Drawing.Point(70, 8);
+            this.lblHeaderStart.Name = "lblHeaderStart";
+            this.lblHeaderStart.Size = new System.Drawing.Size(101, 23);
+            this.lblHeaderStart.TabIndex = 0;
+            this.lblHeaderStart.Text = "Giờ bắt đầu";
+            // 
+            // pnlEnd
+            // 
+            this.pnlEnd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(20)))), ((int)(((byte)(40)))));
+            this.pnlEnd.Controls.Add(this.lblHeaderEnd);
+            this.pnlEnd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pnlEnd.Location = new System.Drawing.Point(290, 160);
+            this.pnlEnd.Name = "pnlEnd";
+            this.pnlEnd.Size = new System.Drawing.Size(220, 90);
+            this.pnlEnd.TabIndex = 4;
+            this.pnlEnd.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlTime_Paint);
+            // 
+            // lblHeaderEnd
+            // 
+            this.lblHeaderEnd.AutoSize = true;
+            this.lblHeaderEnd.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeaderEnd.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblHeaderEnd.ForeColor = System.Drawing.Color.LightGray;
+            this.lblHeaderEnd.Location = new System.Drawing.Point(70, 8);
+            this.lblHeaderEnd.Name = "lblHeaderEnd";
+            this.lblHeaderEnd.Size = new System.Drawing.Size(105, 23);
+            this.lblHeaderEnd.TabIndex = 0;
+            this.lblHeaderEnd.Text = "Giờ kết thúc";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(100, 290);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 55);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "HỦY";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.btnCancel.Paint += new System.Windows.Forms.PaintEventHandler(this.btnCancel_Paint);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Transparent;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(300, 290);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 55);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "LƯU";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            this.btnSave.Paint += new System.Windows.Forms.PaintEventHandler(this.btnSave_Paint);
+
+            // 
+            // FormAddTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(20)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(550, 380);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.pnlEnd);
+            this.Controls.Add(this.pnlStart);
+            this.Controls.Add(this.pnlName);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.lblTitle);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAddTask";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FormAddTask";
+            this.pnlName.ResumeLayout(false);
+            this.pnlName.PerformLayout();
+            this.pnlStart.ResumeLayout(false);
+            this.pnlStart.PerformLayout();
+            this.pnlEnd.ResumeLayout(false);
+            this.pnlEnd.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Panel pnlName;
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.Panel pnlStart;
+        private System.Windows.Forms.Label lblHeaderStart;
+        private System.Windows.Forms.Panel pnlEnd;
+        private System.Windows.Forms.Label lblHeaderEnd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- Forms/FormAddTask.cs
+++ Forms/FormAddTask.cs
@@ -1,20 +1,280 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using Do_an.Models;
 
 namespace Do_an.Forms
 {
-    public partial class FormAddTask: Form
+    public partial class FormAddTask : Form
     {
-        public FormAddTask()
+        public string TaskNameResult { get; private set; }
+        public DateTime StartTimeResult { get; private set; }
+        public DateTime EndTimeResult { get; private set; }
+
+        private DateTime _fixedDate;
+        private DateTime _tempStartTime;
+        private DateTime _tempEndTime;
+        private List<TaskInfo> _existingTasks;
+
+        // Control nhập liệu (Chỉ còn Giờ và Phút)
+        private TextBox txtStartHour, txtStartMin;
+        private TextBox txtEndHour, txtEndMin;
+
+        // Màu sắc
+        private readonly Color clrCyan = Color.FromArgb(0, 229, 255);
+        private readonly Color clrPurple = Color.FromArgb(124, 77, 255);
+        private readonly Color clrPink = Color.FromArgb(233, 30, 99);
+        private readonly Color clrBgPanel = Color.FromArgb(25, 20, 40);
+        private readonly Color clrBorderStart = Color.FromArgb(0, 255, 255);
+        private readonly Color clrBorderEnd = Color.FromArgb(255, 0, 255);
+
+        public FormAddTask(DateTime targetDate, List<TaskInfo> existingTasks = null)
         {
             InitializeComponent();
+            this.KeyPreview = true;
+
+            _fixedDate = targetDate.Date;
+            _existingTasks = existingTasks ?? new List<TaskInfo>();
+
+            DateTime now = DateTime.Now;
+            if (_fixedDate.Date == now.Date)
+                _tempStartTime = _fixedDate.AddHours(now.Hour).AddMinutes(now.Minute);
+            else
+                _tempStartTime = _fixedDate.AddHours(8).AddMinutes(0);
+
+            _tempEndTime = _tempStartTime.AddHours(1);
+
+            SetupDirectInput(pnlStart, true);
+            SetupDirectInput(pnlEnd, false);
+
+            DisplayTimeOnInputs();
+        }
+
+        // --- HÀM TẠO GIAO DIỆN 24H (CĂN GIỮA) ---
+        private void SetupDirectInput(Panel parent, bool isStart)
+        {
+            Font fontNum = new Font("Segoe UI", 18F, FontStyle.Bold);
+
+            // 1. TextBox Giờ (0-23)
+            // [FIX]: X=40 (Căn lề trái để vào giữa khung)
+            TextBox txtHour = CreateTimeTextBox(40, 28, fontNum);
+            txtHour.TextChanged += (s, e) => ValidateNumberInput(txtHour, 0, 23);
+            txtHour.KeyDown += (s, e) => HandleTextBoxKeyDown(txtHour, e, 0, 23);
+            txtHour.Leave += (s, e) => ValidateAndFormat(txtHour, 0, 23);
+            parent.Controls.Add(txtHour);
+
+            // 2. Dấu hai chấm (:)
+            // [FIX]: X=105
+            Label lblSep = new Label();
+            lblSep.Text = ":";
+            lblSep.ForeColor = Color.White;
+            lblSep.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            lblSep.AutoSize = true;
+            lblSep.Location = new System.Drawing.Point(105, 26);
+            parent.Controls.Add(lblSep);
+
+            // 3. TextBox Phút (0-59)
+            // [FIX]: X=120
+            TextBox txtMin = CreateTimeTextBox(120, 28, fontNum);
+            txtMin.TextChanged += (s, e) => ValidateNumberInput(txtMin, 0, 59);
+            txtMin.KeyDown += (s, e) => HandleTextBoxKeyDown(txtMin, e, 0, 59);
+            txtMin.Leave += (s, e) => ValidateAndFormat(txtMin, 0, 59);
+            parent.Controls.Add(txtMin);
+
+            // Không còn Label AM/PM nữa
+            if (isStart) { txtStartHour = txtHour; txtStartMin = txtMin; }
+            else { txtEndHour = txtHour; txtEndMin = txtMin; }
+        }
+
+        private TextBox CreateTimeTextBox(int x, int y, Font f)
+        {
+            TextBox txt = new TextBox();
+            txt.BorderStyle = BorderStyle.None;
+            txt.BackColor = clrBgPanel;
+            txt.ForeColor = Color.White;
+            txt.Font = f;
+            txt.Width = 60;
+            txt.Location = new System.Drawing.Point(x, y);
+            txt.TextAlign = HorizontalAlignment.Center;
+            txt.MaxLength = 2;
+
+            txt.KeyPress += (s, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true;
+            };
+
+            txt.Click += (s, e) => txt.SelectAll();
+
+            return txt;
+        }
+
+        // Xử lý phím Lên/Xuống
+        private void HandleTextBoxKeyDown(TextBox txt, KeyEventArgs e, int min, int max)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                e.SuppressKeyPress = true;
+                int current = int.Parse(string.IsNullOrEmpty(txt.Text) ? "0" : txt.Text);
+
+                if (e.KeyCode == Keys.Up) current++;
+                else current--;
+
+                if (current > max) current = min;
+                if (current < min) current = max;
+
+                txt.Text = current.ToString("00");
+                txt.SelectAll();
+
+                SyncTimeFromUI();
+            }
+        }
+
+        private void ValidateNumberInput(TextBox txt, int min, int max)
+        {
+            if (int.TryParse(txt.Text, out int val))
+            {
+                // Nếu nhập > max (ví dụ 25h), tự sửa về max
+                if (val > max) txt.Text = max.ToString("00");
+            }
+        }
+
+        private void ValidateAndFormat(TextBox txt, int min, int max)
+        {
+            if (int.TryParse(txt.Text, out int val))
+            {
+                if (val > max) val = max;
+                if (val < min) val = min;
+                txt.Text = val.ToString("00");
+            }
+            else
+            {
+                txt.Text = min.ToString("00");
+            }
+            SyncTimeFromUI();
+        }
+
+        private void DisplayTimeOnInputs()
+        {
+            // Hiển thị dạng 24h (HH)
+            txtStartHour.Text = _tempStartTime.ToString("HH");
+            txtStartMin.Text = _tempStartTime.ToString("mm");
+
+            txtEndHour.Text = _tempEndTime.ToString("HH");
+            txtEndMin.Text = _tempEndTime.ToString("mm");
+        }
+
+        private void SyncTimeFromUI()
+        {
+            try
+            {
+                _tempStartTime = GetDateTimeFromUI(txtStartHour, txtStartMin);
+                _tempEndTime = GetDateTimeFromUI(txtEndHour, txtEndMin);
+            }
+            catch { }
+        }
+
+        private DateTime GetDateTimeFromUI(TextBox txtH, TextBox txtM)
+        {
+            int h = int.Parse(string.IsNullOrEmpty(txtH.Text) ? "0" : txtH.Text);
+            int m = int.Parse(string.IsNullOrEmpty(txtM.Text) ? "0" : txtM.Text);
+
+            // Giờ 24h nên không cần xử lý AM/PM, lấy trực tiếp
+            return new DateTime(_fixedDate.Year, _fixedDate.Month, _fixedDate.Day, h, m, 0);
+        }
+
+        // --- NÚT LƯU ---
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SyncTimeFromUI();
+
+            if (string.IsNullOrWhiteSpace(txtTaskName.Text) || txtTaskName.Text == "Tên công việc")
+            {
+                MessageBox.Show("Vui lòng nhập tên công việc!", "Thông báo"); return;
+            }
+            if (_tempEndTime <= _tempStartTime)
+            {
+                MessageBox.Show("Giờ kết thúc phải sau giờ bắt đầu!", "Lỗi thời gian"); return;
+            }
+            if (_fixedDate.Date == DateTime.Now.Date && _tempStartTime < DateTime.Now.AddMinutes(-1))
+            {
+                MessageBox.Show("Không thể đặt giờ quá khứ!", "Lỗi thời gian"); return;
+            }
+            foreach (var task in _existingTasks)
+            {
+                if (_tempStartTime < task.End && _tempEndTime > task.Start)
+                {
+                    MessageBox.Show($"Trùng lịch với: {task.Name}\n({task.Start:HH:mm} - {task.End:HH:mm})", "Lỗi trùng lịch"); return;
+                }
+            }
+
+            TaskNameResult = txtTaskName.Text;
+            StartTimeResult = _tempStartTime;
+            EndTimeResult = _tempEndTime;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        // --- UI HELPERS ---
+        private void lblClose_Click(object sender, EventArgs e) => this.Close();
+        private void btnCancel_Click(object sender, EventArgs e) => this.Close();
+        private void txtTaskName_Enter(object sender, EventArgs e) { if (txtTaskName.Text == "Tên công việc") { txtTaskName.Text = ""; txtTaskName.ForeColor = Color.White; } }
+        private void txtTaskName_Leave(object sender, EventArgs e) { if (string.IsNullOrWhiteSpace(txtTaskName.Text)) { txtTaskName.Text = "Tên công việc"; txtTaskName.ForeColor = Color.Gray; } }
+
+        // --- VẼ TRANG TRÍ (KHÔNG CÒN MŨI TÊN, KHÔNG CÒN ĐƯỜNG KẺ TRẮNG) ---
+        private void pnlTime_Paint(object sender, PaintEventArgs e)
+        {
+            Panel pnl = sender as Panel;
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Vẽ viền Tím
+            DrawRoundedBorder(g, pnl.ClientRectangle, clrPurple, 15);
+
+            // Vẽ Icon Đồng hồ
+            using (Pen p = new Pen(clrCyan, 2))
+            {
+                g.DrawEllipse(p, 8, 42, 16, 16);
+                g.DrawLine(p, 16, 50, 16, 46); g.DrawLine(p, 16, 50, 20, 52);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e); Graphics g = e.Graphics; g.SmoothingMode = SmoothingMode.AntiAlias;
+            using (GraphicsPath p = GetRoundedPath(ClientRectangle, 30)) Region = new Region(p);
+            Rectangle rb = new Rectangle(1, 1, Width - 2, Height - 2);
+            using (GraphicsPath p = GetRoundedPath(rb, 30)) using (LinearGradientBrush b = new LinearGradientBrush(rb, clrBorderStart, clrBorderEnd, 45f)) using (Pen pen = new Pen(b, 3)) g.DrawPath(pen, p);
+            using (LinearGradientBrush b = new LinearGradientBrush(new Point(0, 55), new Point(Width, 55), clrBorderStart, clrBorderEnd)) g.FillRectangle(b, 0, 55, Width, 2);
+        }
+        private void btnSave_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; g.SmoothingMode = SmoothingMode.AntiAlias; Rectangle r = btnSave.ClientRectangle; r.Inflate(-1, -1);
+            using (GraphicsPath p = GetRoundedPath(r, 30)) using (SolidBrush b = new SolidBrush(clrPink)) g.FillPath(b, p);
+            TextRenderer.DrawText(g, btnSave.Text, btnSave.Font, r, Color.White, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        }
+        private void btnCancel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; g.SmoothingMode = SmoothingMode.AntiAlias; Rectangle r = btnCancel.ClientRectangle; r.Inflate(-1, -1);
+            using (GraphicsPath p = GetRoundedPath(r, 30)) using (Pen pen = new Pen(Color.White, 2)) g.DrawPath(pen, p);
+            TextRenderer.DrawText(g, btnCancel.Text, btnCancel.Font, r, Color.White, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        }
+        private void pnlName_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; g.SmoothingMode = SmoothingMode.AntiAlias; DrawRoundedBorder(g, pnlName.ClientRectangle, clrCyan, 15);
+            using (Pen p = new Pen(Color.Gray, 2)) { g.DrawEllipse(p, 15, 18, 14, 14); g.DrawLine(p, 27, 30, 34, 37); }
+        }
+        private void DrawRoundedBorder(Graphics g, Rectangle r, Color c, int rad)
+        {
+            r.Width -= 1; r.Height -= 1; using (GraphicsPath p = GetRoundedPath(r, rad)) using (Pen pen = new Pen(c, 1.5f)) g.DrawPath(pen, p);
+        }
+        private GraphicsPath GetRoundedPath(Rectangle r, int d)
+        {
+            GraphicsPath p = new GraphicsPath(); d *= 2;
+            p.AddArc(r.X, r.Y, d, d, 180, 90); p.AddArc(r.Right - d, r.Y, d, d, 270, 90);
+            p.AddArc(r.Right - d, r.Bottom - d, d, d, 0, 90); p.AddArc(r.X, r.Bottom - d, d, d, 90, 90);
+            p.CloseFigure(); return p;
         }
     }
 }
