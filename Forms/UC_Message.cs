a5515c1a7c956627bb5f5c61e0101aab8499fd00
unknown <vinhgiauten3@gmail.com>
2025-12-28 22:25:49 +0700

    đã hoàn thành form message

---
+++ Forms/UC_Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Do_an.Forms
+{
+    public partial class UC_Message: Form
+    {
+        public UC_Message()
+        {
+            InitializeComponent();
+        }
+    }
+}
